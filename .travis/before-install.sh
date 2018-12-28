7d93bf4c6901593c64ffb2fccb8cfbd7e328ce50
Christos Tsakostas <c.tsakostas@gmail.com>
2018-12-28 19:51:39 +0200

    fix: code signing import

--- .travis/before-install.sh
+++ .travis/before-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_50f149f527b5_key -iv $encrypted_50f149f527b5_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
-    gpg --fast-import codesigning.asc
+    gpg --fast-import .travis/codesigning.asc
 fi
 
