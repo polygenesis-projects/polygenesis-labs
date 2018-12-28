0a65b33dab2174804b593dc326cd0313837d68a7
Christos Tsakostas <c.tsakostas@gmail.com>
2018-12-28 22:01:23 +0200

    fix: sonar maven goal

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    ./mvnw clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar
+    ./mvnw clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar \
+    -Dsonar.host.url=https://sonarcloud.io \
+    -Dsonar.organization=polygenesis-projects \
+    -Dsonar.login=$SONAR_TOKEN
 else
     ./mvnw test -B
 fi
+
